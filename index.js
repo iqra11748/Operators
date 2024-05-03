7bb8f7b649f6461d3825dc79b154da553ea68806
iqra11748 <164212492+iqra11748@users.noreply.github.com>
2024-05-04 02:05:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+//Marksheet
+var englishMarks = 75;
+var mathsMarks = 85;
+var scienceMarks = 95;
+var totalMarks = 225;
+console.log('totalMarks: ', totalMarks);
+var obtainedMarks = englishMarks + mathsMarks + scienceMarks;
+console.log('obtainedMarks:', obtainedMarks);
+var percentage = obtainedMarks / totalMarks * 100;
+console.log('percentage:', percentage);
---
+++ index.ts
@@ -0,0 +1,12 @@
+//Marksheet
+const englishMarks = 75;
+const mathsMarks = 85;
+const scienceMarks = 95;
+
+const totalMarks = 225;
+console.log('totalMarks: ', totalMarks);
+const obtainedMarks = englishMarks + mathsMarks + scienceMarks;
+
+console.log('obtainedMarks:', obtainedMarks);
+const percentage = obtainedMarks / totalMarks * 100;
+console.log('percentage:', percentage);
